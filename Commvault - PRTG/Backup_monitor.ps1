896e2553cabf0381179154a320878dca5a817f26
Valdir Junior <valdirjunior011@hotmail.com>
2022-10-19 14:52:07 +0200

    Upload new files to repository

---
+++ Commvault - PRTG/Backup_monitor.ps1
@@ -0,0 +1,64 @@
+﻿#--------------------------------------------------------------------------
+# Need Module of Powershell on Commvault
+# https://documentation.commvault.com/commvault/v11/article?p=124529_1.htm
+#---------------------------------------------------------------------------
+
+#-------------------------------------------------------------
+#    Monitor for Backups
+#-------------------------------------------------------------
+Param
+(
+    $server = " ",
+    $completedtime = "24"
+)
+$windowsUserName = "Domain\User"
+$windowsPassword = 'Password'
+$secpasswd = ConvertTo-SecureString $windowsPassword -AsPlainText -Force
+$credentials = New-Object System.Management.Automation.PSCredential ($windowsUserName, $secpasswd)
+Connect-CVServer -Server itls0255 -Credential $credentials
+
+#STATUS
+#-------------------------------------------------------------
+$OK = 0
+$WARNING = 1
+$CRITICAL = 2
+#-------------------------------------------------------------
+
+$bkp_run = Get-CVJob -CompletedTime $completedtime | Where-Object { $_.status -eq 'Running' -and $_.destClientName -eq $server }
+$bkp_fail = Get-CVJob -CompletedTime $completedtime | Where-Object { $_.status -eq 'Failed' -and $_.destClientName -eq $server }
+$bkp_comple = Get-CVJob -CompletedTime $completedtime | Where-Object { $_.status -eq 'Completed' -and $_.destClientName -eq $server }
+$bkp_erro = Get-CVJob -CompletedTime $completedtime | Where-Object { $_.status -match 'erro' -and $_.destClientName -eq $server }
+
+## XML Output for PRTG
+Write-Host "<?xml version=`"1.0`" encoding=`"UTF-8`" ?>"  
+Write-Host "<prtg>"
+
+Write-Host "<result>"  
+Write-Host "<channel>Running Backups</channel>"  
+write-host "<unit>#</unit>"
+Write-Host "<value>"$bkp_run.Count "</value>"
+Write-Host "</result>"
+Write-Host "<result>"  
+Write-Host "<channel>Backups w/ Errors</channel>"  
+write-host "<unit>#</unit>"
+Write-Host "<value>"$bkp_erro.Count "</value>"
+Write-Host "<LimitMode>1</LimitMode>"
+Write-Host "<LimitMaxWarning>1</LimitMaxWarning>"
+Write-Host "<LimitWarningMsg>Completed w/ one or more errors</LimitWarningMsg>"
+Write-Host "</result>"
+Write-Host "<result>"  
+Write-Host "<channel>Backups Completed</channel>"  
+write-host "<unit>#</unit>"
+Write-Host "<value>"$bkp_comple.Count "</value>"
+Write-Host "</result>"
+Write-Host "<result>"  
+Write-Host "<channel>Backup Failed</channel>"  
+write-host "<unit>#</unit>"
+Write-Host "<value>"$bkp_fail.Count "</value>"
+Write-Host "<LimitMode>1</LimitMode>"
+Write-Host "<LimitMaxError>1</LimitMaxError>"
+Write-Host "<LimitWarningMsg>Backup Failed </LimitWarningMsg>"
+Write-Host "</result>"
+Write-Host "<Text> Log saved in \\itls0255\f$\Monitoramento Prtg </Text>"
+
+Write-Host "</prtg>"
---
+++ Windows/DiskSpace.ps1
@@ -0,0 +1,56 @@
+﻿#-------------------------------------------------------------
+# Check Disk Space using label reference "Support Mount Point"
+#-------------------------------------------------------------
+Param
+(
+    [string]$strcomputer = " ",
+    [string]$label = "",
+    [int]$war = 80,
+    [int]$crit = 95
+
+)
+#-------------------------------------------------------------
+#STATUS
+#-------------------------------------------------------------
+$OK = 0
+$WARNING = 1
+$CRITICAL = 2
+$UNKNOWN = 3
+$MSG = ""
+#-------------------------------------------------------------
+#Check Server Volumes
+#-------------------------------------------------------------
+If (Test-Connection -ComputerName $strcomputer -Count 2 -Quiet) {
+    $objects = Get-WmiObject -computer $strcomputer win32_volume | Select-Object label, name, @{Name = ”Capacity”; expression = { [math]::round((($_.Capacity / 1024) / 1024)) } }, @{Name = ”Freespace”; expression = { [math]::round((($_.Freespace / 1024) / 1024)) } } | Where-Object { $_.label -match $label }
+    $rcrit = @()
+    $rwar = @()
+    $rok = @()
+    Foreach ($object in $objects) {
+        $per = 100 - ([math]::round($object.Freespace * 100 / $object.capacity))  
+        if ($per -igt $crit) {
+            $rcrit += "$($Object.label) $($per)% in use and $([math]::round($object.Freespace)) GB free (CRITICAL)"
+        }
+        elseif ($per -ilt $crit -and $per -igt $war) {
+            $rwar += "$($Object.label) $($per)% in use and $([math]::round($object.Freespace)) GB free (WARNING)"
+        }
+        else {
+            $rok += "$($Object.label) $($per)% in use and $([math]::round($object.Freespace)) GB free (OK)"
+        }
+    }
+    $MSG = "$($rcrit.count) CRITICAL / $($rwar.Count) WARNING / $($rok.count) OK alerts found"
+    
+    if ($rcrit.count -gt 0) {
+        write-host "2:CRITICAL - $($MSG) `n$($rcrit  -join "`n")"# `n$($rwar -join "`n")"
+        exit $CRITICAL
+    }
+    if ($rwar.count -gt 0) {
+        write-host "1:WARNING - $($MSG) `n$([string]$rwar)"
+        exit $WARNING
+    }
+    write-host "0:OK - $($MSG)"# `n$([string]$rok)"
+    exit $OK
+}
+else {
+    write-output "2:CRITICAL - $strcomputer NOT FOUND"
+    exit $CRITICAL
+}

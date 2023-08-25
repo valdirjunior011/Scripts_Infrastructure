121d920cb62339ebbce81a6ae2bd68815190175a
Junior <Valdir.Junior@voith.com>
2023-08-25 11:04:08 +0200

    add s3 bucket for vpc flow

--- test_containers/terraform/test.tf
+++ test_containers/terraform/test.tf
@@ -17,6 +17,30 @@ data "aws_ami" "latest_ami" {
 
   owners = ["099720109477"]
 }
+resource "aws_s3_bucket" "vpc_flow_logs" {
+  bucket = "my-vpc-flow"
+  acl = "private"
+  
+  versioning {
+    enabled = true
+  }
+  lifecycle {
+    prevent_destroy = true
+    rule {
+      id = "expire-old-logs"
+      status = "Enable"
+      prefix = ""
+      enabled = true
+      transitions {
+        days = 30
+        storage_class = "GLACIER"
+      }
+      expiration {
+        days = 90
+      }
+    }
+  }
+}
 
 resource "aws_vpc" "example_vpc" {
   cidr_block = var.allowed_cidr_blocks
